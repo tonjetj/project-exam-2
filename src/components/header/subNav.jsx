66bec5e001b78f19c48402b0f29876632e4a0814
tonjetj <tontotjen@gmail.com>
2023-05-06 02:53:30 +0200

    added styling

--- src/components/header/subNav.jsx
+++ src/components/header/subNav.jsx
@@ -5,23 +5,25 @@ import ExploreRoundedIcon from '@mui/icons-material/ExploreRounded';
 import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
+
+
 const SubNavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #151718;
-  padding: 10px 20px;
+  padding: 10px 20px; 
   position: fixed;
   bottom: 0;
   width: 100%;
   z-index: 1;
   box-sizing: border-box;
-  border-radius: 20px 20px 0px 0px; 
+  border-radius: 60px 60px 0px 0px; 
 
   @media screen and (min-width: 768px) {
     position: static;
     box-shadow: none;
-    border-radius: 0px 0px 20px 20px; 
+    border-radius: 0px 0px 60px 60px; 
     width: 50%; 
     justify-content: space-between; 
     margin: 0 auto; 
@@ -33,17 +35,21 @@ const SubNavigationItem = styled.a`
   font-weight: light;
   cursor: pointer;
   text-align: center;
-  font-size: 14px; 
-  margin: auto; 
+  font-size: 12px; 
+  margin: 0 auto; 
 `;
+
+const Sp = styled.p`
+padding: 0px; 
+margin-top: 0px; `
 
 function SubNavigation() {
   return (
     <SubNavWrapper>
-      <SubNavigationItem><HomeRoundedIcon/><p>Home</p></SubNavigationItem>
-      <SubNavigationItem><ExploreRoundedIcon/><p>Explore</p></SubNavigationItem>
-      <SubNavigationItem><FavoriteRoundedIcon/><p>Favourites</p></SubNavigationItem>
-      <SubNavigationItem><AccountCircleRoundedIcon/><p>Log In</p></SubNavigationItem>
+      <SubNavigationItem><HomeRoundedIcon/><Sp>Home</Sp></SubNavigationItem>
+      <SubNavigationItem><ExploreRoundedIcon/><Sp>Explore</Sp></SubNavigationItem>
+      <SubNavigationItem><FavoriteRoundedIcon/><Sp>Favourites</Sp></SubNavigationItem>
+      <SubNavigationItem><AccountCircleRoundedIcon/><Sp>Log In</Sp></SubNavigationItem>
     </SubNavWrapper>
   );
 };
