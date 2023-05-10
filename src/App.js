b8c3818f3d18c467bb13669aba4416aeaea7235e
tonjetj <tontotjen@gmail.com>
2023-05-10 16:46:20 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header/index.jsx";
+import Header from "./components/header/Header.jsx";
 function App() {
   return (
     <div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import SubNavigation from "./subNav";
+import SubNavigation from "./SubNav";
 import SearchBar from "./SearchBar";
 import logo from "../../assets/logo.png";
 
