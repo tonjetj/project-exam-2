7df8df736e77d2b6bfa504c9880d49a920469bf8
tonjetj <tontotjen@gmail.com>
2023-05-10 18:28:32 +0200

    adds api and router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
+import LandingPage from "./pages/landingPage";
+import VenuesPage from "./pages/venues";
+
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/venues" element={<VenuesPage />} />
+          {/* <Route path="/favourites" element={<FavouritesPage />} />
+          <Route path="/login" element={<LoginPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/api/apiBase.js
+++ src/api/apiBase.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+// export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const API_BASE_URL = "https://api.noroff.dev/api/v1";
