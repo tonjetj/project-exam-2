e251fe910ce54db236a4d2a79b7f9f12b2dc8d92
tonjetj <tontotjen@gmail.com>
2023-05-18 18:41:13 +0200

    directs user to dashboard if successful login

--- src/components/loginForm/index.jsx
+++ src/components/loginForm/index.jsx
@@ -1,55 +1,78 @@
+import React, {useState} from "react";
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
 import { yupResolver } from '@hookform/resolvers/yup';
-import API_BASE_URL from "../../api/apiBase";
+import { API_BASE_URL } from "../../api/apiBase";
+import * as S from "./index.styles";
+import { InputAdornment } from "@mui/material";
+import { loginSchema } from "./schema";
 
 
-
-const loginSchema = yup.object().shape({
-    email: yup.string()
-  .email('Please enter a valid email address.')
-  .matches(/^(.*)@(stud\.)?noroff\.no$/, 'Use a valid email ending with "@stud.noroff.no" or "@noroff.no"')
-  .required('Email is required.'),
-    password: yup.string().required('Password is required.'),
+function LoginForm({onClose, navigate, onSuccess}) {
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(loginSchema),
   });
 
-  function LoginForm() {
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(loginSchema),
-      });
-  
-    const onSubmit = async (data) => {
-        try {
-            const postData = {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-              };
-          const response = await fetch(`${API_BASE_URL}/holidaze/auth/login`, postData);
-          console.log(response); 
-          const json = await response.json(); 
-          console.log(json);
-          localStorage.setItem('accessToken', response.accessToken);
+  const onSubmit = async (data) => {
+    try {
+      setIsLoggingIn(true);
 
-        } catch (error) {
-          console.log(error);
-        }
+      const postData = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
       };
-      
-  
-      return (
-        <form onSubmit={handleSubmit(onSubmit)}>
 
-          <input {...register('email')} />
-          {errors.email && <p>{errors.email.message}</p>}
-          <input type="password" {...register('password')} />
-          {errors.password && <p>{errors.password.message}</p>}
-          <button type="submit">Log In</button>
-        </form>
-      );
-  }
+      const response = await fetch(`${API_BASE_URL}/holidaze/auth/login`, postData);
+      const json = await response.json();
 
-  export default LoginForm;
+       if (response.ok) {
+        localStorage.setItem('accessToken', json.accessToken);
+        onClose();
+        onSuccess(json.userData);
+        navigate("/dashboard");
+      } else {
+        console.log(json); 
+      }
+
+        setIsLoggingIn(false);
+    } catch (error) {
+        console.log(error);
+        setIsLoggingIn(false); 
+    }
+  };
+
+
+  return (
+    <S.FormWrapper onSubmit={handleSubmit(onSubmit)}>
+      <h4>Log in</h4>
+      <S.FormInput {...register("email")} placeholder="example@stud.noroff.no" variant="standard" label="Email" InputProps={{
+    startAdornment: (
+      <InputAdornment position="start">
+        <S.Email />
+      </InputAdornment>
+    ),
+  }} />
+      {errors.email && <S.ErrorMessage>{errors.email.message}</S.ErrorMessage>}
+      <S.FormInput type="password" {...register("password")} placeholder="********" variant="standard" label="Password" InputProps={{
+    startAdornment: (
+      <InputAdornment position="start">
+        <S.Password />
+      </InputAdornment>
+    ),
+  }} />
+      {errors.password && <S.ErrorMessage>{errors.password.message}</S.ErrorMessage>}
+      <S.SubmitButton type="submit" disabled={isLoggingIn}>
+        {isLoggingIn ? "Logging In..." : "Log In"}
+      </S.SubmitButton>
+        <S.RegisterText onClick={onClose}>
+          Don't have a profile? <S.SLink to="/register">Register</S.SLink>
+        </S.RegisterText>
+    </S.FormWrapper>
+  );
+}
+
+export default LoginForm;
