06c04666212d87247c221b4886c8326eee67cf25
tonjetj <tontotjen@gmail.com>
2023-05-28 23:11:54 +0200

    adds footer

---
+++ src/components/footer/index.styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const Footer = styled.div`
+background-color: #000; 
+padding: 20px 0px; 
+margin-top: 50px; 
+text-align: center; 
+color: #fff; 
+@media screen and (max-width: 798px){
+    padding-bottom: 80px; 
+}
+`
+
+export const Logo = styled.img`
+max-width: 150px; 
+`
