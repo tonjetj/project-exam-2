1a6b1a03b616e78ca327bc9ae8229273bb355cbe
tonjetj <tontotjen@gmail.com>
2023-05-29 16:58:43 +0200

    Updates

--- src/api/apiBase.js
+++
@@ -1,3 +0,0 @@
-// export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
-
-export const API_BASE_URL = "https://api.noroff.dev/api/v1";
--- src/components/loginForm/schema.jsx
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const loginSchema = yup.object().shape({
-    email: yup.string()
-      .email('Please enter a valid email address.')
-      .matches(/^(.*)@(stud\.)?noroff\.no$/, 'Email is required.')
-      .required('Email is required.'),
-    password: yup.string().required('Password is required.'),
-  });
